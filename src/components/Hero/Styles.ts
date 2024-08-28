0ff50214f4b2009143c81a3a6633e22390110c3a
Josyway Paulo <josywaypaulo@gmail.com>
2024-08-28 18:08:59 -0300

    finalização da converção

---
+++ src/components/Hero/Styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+const Formulario = styled.form`
+  height: 360px;
+  width: 100%;
+  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
+  background-size: cover;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  ::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${(props) => props.theme.corPrincipal};
+    content: '';
+    opacity: 0.7;
+  }
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 24px 0;
+  }
+`
+export default Formulario
+
+export const Title = styled.h2`
+  color: ${(props) => props.theme.corTexto};
+  font-family: Gloock, serif;
+  font-size: 48px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
+`
